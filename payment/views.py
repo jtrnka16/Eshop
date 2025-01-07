95821eaf9d9f90d75759de21b4602cfb62c788c4
JakubTrnka <jakubtrnka16@gmail.com>
2025-01-07 20:27:23 +0100

    Sending email after purchase functionality

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from store.models import Product
 from . models import ShippingAddress, Order, OrderItem
 
 from cart.cart import Cart
 
 from django.http import JsonResponse
+
+from django.core.mail import send_mail
+
+from django.conf import settings
 
 def checkout(request):
 
@@ -90,10 +95,22 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                                      price=item['price'], user=request.user)
+
+                product_list.append(item['product'])
+
+                all_products = product_list
+
+                # Email order
+
+            send_mail('Order received', 'Hello!' + '\n\n' + 'Thank you for your order!' + '\n\n' +
+                      'Please see your order for details!' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $'
+                      + str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False)
 
         # 2) Create order -> Guest user without account
 
@@ -103,9 +120,21 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                                          price=item['price'])
+
+                product_list.append(item['product'])
+
+            all_products = product_list
+
+            # Email order
+
+            send_mail('Order received', 'Hello!' + '\n\n' + 'Thank you for your order!' + '\n\n' +
+                'Please see your order for details!' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: $'
+                 + str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False)
 
 
     order_success = True
